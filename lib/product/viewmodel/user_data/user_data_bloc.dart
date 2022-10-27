dedbb5caa31972ab7579b8d50fa29370f7669244
Ümit Kar <umitkar08@gmail.com>
2022-10-27 22:46:51 +0300

    Update user_data_bloc

--- lib/product/viewmodel/user_data/user_data_bloc.dart
+++ lib/product/viewmodel/user_data/user_data_bloc.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:bkapp/domain/models/user_model.dart';
 import 'package:bkapp/domain/repository/mock_api_repository.dart';
 import 'package:equatable/equatable.dart';
@@ -11,8 +9,10 @@ part 'user_data_state.dart';
 
 class UserDataBloc extends Bloc<UserDataEvent, UserDataState> {
   final MockApiRepository _repository;
-  UserDataBloc(this._repository) : super(const UserDataState()) {
-    //onUserDataFetchEvent func 
+  UserDataBloc(MockApiRepository repository)
+      : _repository = repository,
+        super(const UserDataState()) {
+    //onUserDataFetchEvent func
     on<UserDataFetchEvent>(_onUserDataFetch);
   }
 
@@ -21,16 +21,16 @@ class UserDataBloc extends Bloc<UserDataEvent, UserDataState> {
       UserDataFetchEvent event, Emitter<UserDataState> emit) async {
     try {
       if (state.status == FetchStatus.initial) {
-      final result = await _repository.fetchData();
-      return emit(state.copyWith(status: FetchStatus.success, users: result));
-    }
+        final result = await _repository.fetchData();
+        return emit(state.copyWith(status: FetchStatus.success, users: result));
+      }
 
-    final users = await _repository.fetchData();
-    users!.isEmpty
-        ? emit(state.copyWith(status: FetchStatus.failure))
-        : emit(state.copyWith(
-            status: FetchStatus.success,
-            users: List.of(state.users)..addAll(users)));
+      final users = await _repository.fetchData();
+      users!.isEmpty
+          ? emit(state.copyWith(status: FetchStatus.failure))
+          : emit(state.copyWith(
+              status: FetchStatus.success,
+              users: List.of(state.users)..addAll(users)));
     } catch (_) {
       emit(state.copyWith(status: FetchStatus.failure));
     }
